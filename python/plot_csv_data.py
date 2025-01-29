c6ef3fa13312eec989b40f7a9f15941a769a2a6f
Jussi Karttunen <jussi.m.karttunen@gmail.com>
2025-01-29 17:01:16 +0200

    Add example about generating sine wave data into CSV file and plot that with Python

---
+++ python/plot_csv_data.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Specify CSV file name and location
+csv_file_path = "sine_wave_data_points.csv"
+
+# Read the CSV file
+data = pd.read_csv(csv_file_path)
+
+# Assume "Time[s]" is the x-axis data for the figure
+data.set_index("Time[s]", inplace=True)
+
+# Plot the data into figure
+data.plot()
+plt.show()
---
+++ sine_wave_csv_data.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <fstream>
+#include <numbers>
+
+int main()
+{
+    // Parameters for the creating the sine wave data
+    constexpr double time_step_s = 1.0e-3;
+    constexpr double time_data_duration_s = 3.0;
+    constexpr double frequency_wave_hz = 2.0;
+
+    // Sine wave data points are logged into this csv file
+    std::ofstream csv_file{"sine_wave_data_points.csv"};
+
+    // Write signal name header row into the file
+    csv_file << "Time[s]," << "Sine_wave" << std::endl;
+
+    // Data point generation loop
+    for (double time_elapsed_s = 0.0; time_elapsed_s <= time_data_duration_s; time_elapsed_s += time_step_s)
+    {
+        constexpr double angular_frequency_rad_s = 2.0f * std::numbers::pi * frequency_wave_hz;
+        const double angle_rad = angular_frequency_rad_s * time_elapsed_s;
+        const double sine_wave = std::sin(angle_rad);
+
+        csv_file << time_elapsed_s << "," << sine_wave << std::endl;
+    }
+
+    // Remember to close the file handle when done
+    csv_file.close();
+    return 0;
+}
