ec936bb178bfced3a9b3bf5ca099f5f6cb319dd0
Jussi Karttunen <jussi.m.karttunen@gmail.com>
2025-02-01 21:55:42 +0200

    Making tree like project folder structure with CMake

--- source/sine_wave/sine_wave_csv_data.cpp
+++ source/sine_wave/sine_wave_csv_data.cpp
@@ -5,9 +5,9 @@
 int main()
 {
     // Parameters for the creating the sine wave data
-    constexpr double time_step_s          = 1.0e-3;
-    constexpr double time_data_duration_s = 3.0;
-    constexpr double frequency_wave_hz    = 2.0;
+    constexpr double time_step_s         = 1.0e-3;
+    constexpr double time_total_length_s = 3.0;
+    constexpr double frequency_wave_hz   = 2.0;
 
     // Sine wave data points are logged into this csv file
     std::ofstream csv_file{"sine_wave_data_points.csv"};
@@ -16,13 +16,19 @@ int main()
     csv_file << "Time[s]," << "Sine_wave" << std::endl;
 
     // Data point generation loop
-    for (double time_elapsed_s = 0.0; time_elapsed_s <= time_data_duration_s; time_elapsed_s += time_step_s)
+    double time_elapsed_s = 0.0;
+    while (time_elapsed_s <= time_total_length_s)
     {
-        constexpr double angular_frequency_rad_s = 2.0f * std::numbers::pi * frequency_wave_hz;
+        constexpr double angular_frequency_rad_s = 2.0 * std::numbers::pi * frequency_wave_hz;
         const double angle_rad                   = angular_frequency_rad_s * time_elapsed_s;
         const double sine_wave                   = std::sin(angle_rad);
 
+        // Write the data points into the file
         csv_file << time_elapsed_s << "," << sine_wave << std::endl;
+
+        // Increment the elapsed time as a last action in the loop instead of first,
+        // to not skip the initial time is zero (=0.0) step
+        time_elapsed_s += time_step_s;
     }
 
     // Remember to close the file handle when done
