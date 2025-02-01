ad59d2611eac96c2d199f6e62bb81c629307fb7c
Jussi Karttunen <jussi.m.karttunen@gmail.com>
2025-02-01 19:58:26 +0200

    Reformat files with the new clang-format file

--- sine_wave_csv_data.cpp
+++ sine_wave_csv_data.cpp
@@ -5,9 +5,9 @@
 int main()
 {
     // Parameters for the creating the sine wave data
-    constexpr double time_step_s = 1.0e-3;
+    constexpr double time_step_s          = 1.0e-3;
     constexpr double time_data_duration_s = 3.0;
-    constexpr double frequency_wave_hz = 2.0;
+    constexpr double frequency_wave_hz    = 2.0;
 
     // Sine wave data points are logged into this csv file
     std::ofstream csv_file{"sine_wave_data_points.csv"};
@@ -19,8 +19,8 @@ int main()
     for (double time_elapsed_s = 0.0; time_elapsed_s <= time_data_duration_s; time_elapsed_s += time_step_s)
     {
         constexpr double angular_frequency_rad_s = 2.0f * std::numbers::pi * frequency_wave_hz;
-        const double angle_rad = angular_frequency_rad_s * time_elapsed_s;
-        const double sine_wave = std::sin(angle_rad);
+        const double angle_rad                   = angular_frequency_rad_s * time_elapsed_s;
+        const double sine_wave                   = std::sin(angle_rad);
 
         csv_file << time_elapsed_s << "," << sine_wave << std::endl;
     }
--- test_example.cpp
+++ test_example.cpp
@@ -7,7 +7,7 @@ TEST_CASE("Test case name")
     SECTION("Section name 1")
     {
         // Example how to check int and boolen values
-        constexpr int result_int = 1;
+        constexpr int result_int   = 1;
         constexpr int expected_int = 1;
         REQUIRE(result_int == expected_int);
 
@@ -21,9 +21,9 @@ TEST_CASE("Test case name")
     SECTION("Section name 2")
     {
         // Example how to check float and double values
-        constexpr float result_float = 1.0f;
+        constexpr float result_float   = 1.0f;
         constexpr float expected_float = 1.0f;
-        constexpr float error_margin = 0.001f;
+        constexpr float error_margin   = 0.001f;
         REQUIRE_THAT(result_float, WithinAbs(expected_float, error_margin));
     }
 }
