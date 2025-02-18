8a145237cc3016fdda89a8545fa8bd6cb3e1e1c1
Jussi Karttunen <jussi.m.karttunen@gmail.com>
2025-02-18 18:03:59 +0200

    Start adding space rocket example

---
+++ source/rocket/rocket.hpp
@@ -0,0 +1,54 @@
+//---------------------------------------------------------------------------------------------------------------------
+// Rocket
+//---------------------------------------------------------------------------------------------------------------------
+
+#pragma once
+
+//---------------------------------------------------------------------------------------------------------------------
+// PUBLIC TYPE DEFINITIONS
+//---------------------------------------------------------------------------------------------------------------------
+
+struct RocketPars_t
+{
+    double time_step_simulation_s = 0.0;
+    double mass_rocket_kg         = 0.0;
+};
+
+struct RocketState_t
+{
+    double distance_m = 0.0;
+    double speed_m_s  = 0.0;
+};
+
+//---------------------------------------------------------------------------------------------------------------------
+// PUBLIC CLASS DEFINITIONS
+//---------------------------------------------------------------------------------------------------------------------
+
+class Rocket_t
+{
+  public:
+    Rocket_t(const RocketPars_t& pars) :
+        m_pars{pars}
+    {}
+
+    void SetSpeed_m_s(const double speed_m_s)
+    {
+        m_state.speed_m_s = speed_m_s;
+    }
+
+    double UpdateDistance_m()
+    {
+        // Distance is integral of speed (s = ∫vdt)
+        m_state.distance_m += m_state.speed_m_s * m_pars.time_step_simulation_s;
+        return m_state.distance_m;
+    }
+
+    double GetDistance_m() const
+    {
+        return m_state.distance_m;
+    }
+
+  private:
+    RocketPars_t m_pars;
+    RocketState_t m_state = {};
+};
---
+++ source/rocket/test_rocket.cpp
@@ -0,0 +1,40 @@
+#include <catch2/catch_all.hpp>
+
+#include "rocket.hpp"
+
+using namespace Catch::Matchers;
+
+static auto Near(const double expected_value)
+{
+    constexpr double allowed_error_tolerance = 1.0e-3;
+    return WithinAbs(expected_value, allowed_error_tolerance);
+}
+
+TEST_CASE("Rocket traveled distance")
+{
+    SECTION("With constant speed")
+    {
+        constexpr double time_step_simulation_s = 1.0e-3;
+        constexpr double time_total_length_s    = 100.0;
+
+        constexpr double speed_rocket_m_s = 10.0;
+
+        Rocket_t rocket{
+            {.time_step_simulation_s = 1.0e-3,
+             .mass_rocket_kg         = 1000.0}
+        };
+
+        rocket.SetSpeed_m_s(speed_rocket_m_s);
+
+        // Rocket travelling simulation loop
+        double time_elapsed_s = 0.0;
+        while (time_elapsed_s <= time_total_length_s)
+        {
+            rocket.UpdateDistance_m();
+            time_elapsed_s += time_step_simulation_s;
+        }
+
+        constexpr double expected_distance_m = time_total_length_s * speed_rocket_m_s;
+        REQUIRE_THAT(rocket.GetDistance_m(), Near(expected_distance_m));
+    }
+}
