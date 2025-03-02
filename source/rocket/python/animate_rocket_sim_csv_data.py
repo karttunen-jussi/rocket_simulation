6d2473ed7c36a56ed41b7d2f89f7c5ea3f32f8d1
Jussi Karttunen <jussi.m.karttunen@gmail.com>
2025-03-02 13:36:29 +0200

    Animate the simulated data with python in 2D grid

--- source/rocket/python/animate_rocket_sim_csv_data.py
+++ source/rocket/python/animate_rocket_sim_csv_data.py
@@ -8,42 +8,44 @@ csv_file_path = "rocket_sim_data_points.csv"
 # Read the CSV file
 data = pd.read_csv(csv_file_path)
 
-# Set "Time[s]" as the x-axis data for the figure
-data.set_index("Time[s]", inplace=True)
+time_step_data_ms           = 1.0
+time_interval_animation_ms  = 100.0
+count_data_points_per_frame = int(time_interval_animation_ms / time_step_data_ms)
 
-number_of_subplots = len(data.columns)
-fig, axes = plt.subplots(number_of_subplots, 1, sharex=True)
+x_axis_signal = "Position_x[m]"
+y_axis_signal = "Position_y[m]"
+
+x_axis_data = data[x_axis_signal]
+y_axis_data = data[y_axis_signal]
+
+fig, axis = plt.subplots(1, 1)
 fig.set_tight_layout(True)
+line_object = axis.plot([], [])
 
-plt.xlabel("Time[s]")
+plot_margin = 10.0
+axis.set_xlim(min(x_axis_data) - plot_margin, max(x_axis_data) + plot_margin)
+axis.set_ylim(min(y_axis_data) - plot_margin, max(y_axis_data) + plot_margin)
+axis.grid()
 
-all_plot_line_objects = []
-for n in range(number_of_subplots):
-    axis_of_subplot = axes[n]
-    line_object = axis_of_subplot.plot([], [])
-    all_plot_line_objects.append(line_object)
-    axis_of_subplot.set_ylabel(data.columns[n])
-    axis_of_subplot.grid()
+plt.xlabel(x_axis_signal)
+plt.ylabel(y_axis_signal)
 
 # Update function for animation
 def UpdateAnimation(frame_number):
-    count_plotted_points = (frame_number + 1) * 100
-    for n in range(number_of_subplots):
-        line_object = all_plot_line_objects[n]
-        signal_data = data.iloc[0:count_plotted_points, n]
-        time_data   = data.index[0:count_plotted_points]
-        line_object[0].set_data(time_data, signal_data)
+    count_plotted_points = (frame_number + 1) * count_data_points_per_frame
 
-    for k in range(number_of_subplots):
-        axis_of_subplot = axes[k]
-        axis_of_subplot.relim()
-        axis_of_subplot.autoscale_view()
-        axis_of_subplot.set_xlim(time_data[0], time_data[-1])
+    x_axis_plot_data = x_axis_data[0:count_plotted_points]
+    y_axis_plot_data = y_axis_data[0:count_plotted_points]
+    line_object[0].set_data(x_axis_plot_data, y_axis_plot_data)
 
-    return all_plot_line_objects
+    return line_object
 
 # Animate the plot
-ani = animation.FuncAnimation(fig, UpdateAnimation, blit=False, interval=100, cache_frame_data=False)
+ani = animation.FuncAnimation(fig      = fig,
+                              func     = UpdateAnimation,
+                              interval = int(time_interval_animation_ms),
+                              blit     = False,
+                              cache_frame_data = False)
 
 # Display the plot
 plt.show()
