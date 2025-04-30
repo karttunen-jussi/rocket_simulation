79d4c4a59092a2a001c5029227d069c828df0e94
Vesa Karttunen <vesakarttunen@hotmail.fi>
2025-04-30 13:54:30 +0300

    Start adding PyTest for integration testing

---
+++ tests/test_socket_integration.py
@@ -0,0 +1,38 @@
+
+import socket
+import struct
+import subprocess
+import time
+
+
+def start_rocket():
+    exe_path = r"E:\Koodausharjoittelu\CPracBuild\source\rocket\RocketSocketServer.exe"
+    return subprocess.Popen(
+        [exe_path],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+
+def test_rocket_socket_integration():
+    
+    rocket = start_rocket()
+    time.sleep(1)
+
+    try:
+        port_tcp_socket = 1234
+        tcp_socket_sim = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_socket_sim.connect(("localhost", port_tcp_socket))
+
+        data_send_raw = struct.pack("<2d", 5.0, 15.0)
+        tcp_socket_sim.sendall(data_send_raw)
+
+        data_received_raw = tcp_socket_sim.recv(16)
+        data_converted = struct.unpack("<2d", data_received_raw)
+        print("Got back from Rocket:", data_converted)
+
+        assert len(data_converted) == 2
+        assert all(isinstance(i, float) for i in data_converted)
+
+    finally:
+        tcp_socket_sim.close()
+        subprocess.call(["taskkill", "/F", "/T", "/PID", str(rocket.pid)])
